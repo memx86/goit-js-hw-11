39775c64e9441ee78095bc9abc8e7f6c5b640c73
Yevhen Malyshko <memx86@gmail.com>
2022-01-17 10:37:21 +0200

    load more btn

--- src/js/search.js
+++ src/js/search.js
@@ -7,6 +7,7 @@ const axios = require('axios');
 const refs = {
   form: document.querySelector('#search-form'),
   gallery: document.querySelector('.gallery__container'),
+  moreBtn: document.querySelector('.load-more'),
 };
 
 const API_KEY = '25272385-d3b781fb1902e693cd197cf56';
@@ -14,9 +15,20 @@ const BASE_URL = 'https://pixabay.com/api/';
 const DEFAULT_QUERY = '&per_page=40&image_type=photo&orientation=horizontal&safesearch=true';
 let query = '';
 let page = 1;
+let gallery;
 
 refs.form.addEventListener('submit', onSearch);
+refs.moreBtn.addEventListener('click', onMoreBtnClick);
 
+function onSearch(e) {
+  e.preventDefault();
+  page = 1;
+  clearGalleryMarkup();
+  query = e.target.searchQuery.value;
+  const url = `${BASE_URL}?key=${API_KEY}&q=${query}&page=${page}${DEFAULT_QUERY}`;
+  axios.get(url).then(onSuccess).catch(onError);
+  gallery = new SimpleLightbox('.gallery__container a');
+}
 function onSuccess(response) {
   const cards = response.data.hits;
   if (cards.length === 0) {
@@ -28,16 +40,24 @@ function onSuccess(response) {
 
   Notify.success(`Hooray! We found ${totalHits} images.`);
   refs.gallery.insertAdjacentHTML('beforeend', cardsMarkup);
-  let gallery = new SimpleLightbox('.gallery__container a');
+  page += 1;
   gallery.refresh();
+  showButton();
 }
 function onError() {
   Notify.failure('Sorry, there is no response from server. Please try again.');
 }
-function onSearch(e) {
-  page = 1;
-  e.preventDefault();
-  query = e.target.searchQuery.value;
+function onMoreBtnClick() {
+  hideButton();
   const url = `${BASE_URL}?key=${API_KEY}&q=${query}&page=${page}${DEFAULT_QUERY}`;
   axios.get(url).then(onSuccess).catch(onError);
+}
+function hideButton() {
+  refs.moreBtn.classList.add('is-hidden');
+}
+function showButton() {
+  refs.moreBtn.classList.remove('is-hidden');
+}
+function clearGalleryMarkup() {
+  refs.gallery.innerHTML = '';
 }
